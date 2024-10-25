07694bb46c6e689dc929bb1eb37dff758b91c85b
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-25 16:55:22 +0200

    Extension finished

--- backend-ai/src/main/java/org/hobbies/repositories/HobbyRepository.java
+++ backend-ai/src/main/java/org/hobbies/repositories/HobbyRepository.java
@@ -1,4 +1,7 @@
 package org.hobbies.repositories;
 
-public class HobbyRepository {
+import org.hobbies.models.Hobby;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HobbyRepository extends JpaRepository<Hobby, Integer> {
 }
